aa1d2ffdffb7b39ab63f3e3ee6f7f9d7734e3432
AET Technology <aettechnology5@gmail.com>
2026-06-27 01:56:06 +0200

    Create collections.js

---
+++ commerce-lot1/src/commerce/core/config/collections.js
@@ -0,0 +1,26 @@
+export const COLLECTIONS = {
+  TENANTS: 'tenants',
+  SETTINGS: 'settings',
+  STORES: 'stores',
+  CATEGORIES: 'categories',
+  PRODUCTS: 'products',
+  INVENTORY: 'inventory',
+  CUSTOMERS: 'customers',
+  SUPPLIERS: 'suppliers',
+  EMPLOYEES: 'employees',
+  ROLE_BINDINGS: 'roleBindings',
+  ORDERS: 'orders',
+  SALES: 'sales',
+  PURCHASES: 'purchases',
+  CASH_SESSIONS: 'cashSessions',
+  INVENTORY_COUNTS: 'inventoryCounts',
+  STOCK_MOVEMENTS: 'stockMovements',
+  REPORTS_DAILY: 'reportsDaily',
+  REPORTS_MONTHLY: 'reportsMonthly',
+};
+
+export const SETTINGS_DOCS = {
+  GENERAL: 'general',
+  BRANDING: 'branding',
+  POS: 'pos',
+};

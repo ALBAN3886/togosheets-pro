f32fa5ef46aa1696432ae8f94051fb2df682212b
AET Technology <aettechnology5@gmail.com>
2026-06-27 08:07:12 +0200

    feat: add commerce-lot1 module — commerce-lot5/src/commerce/core/realtime/index.js

---
+++ commerce-lot5/src/commerce/core/realtime/index.js
@@ -0,0 +1,7 @@
+export * from './watch-products.js';
+export * from './watch-orders.js';
+export * from './watch-stock.js';
+export * from './watch-stats.js';
+export * from './watch-sales.js';
+export * from './watch-cash-sessions.js';
+export * from './watch-stock-movements.js';

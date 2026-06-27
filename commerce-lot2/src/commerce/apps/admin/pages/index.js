9a47584fd3720eba003f5c7a96be180072df67fb
AET Technology <aettechnology5@gmail.com>
2026-06-27 02:38:34 +0200

    feat: add commerce-lot1 module — commerce-lot2/src/commerce/apps/admin/pages/index.js

---
+++ commerce-lot2/src/commerce/apps/admin/pages/index.js
@@ -0,0 +1,7 @@
+export * from './dashboard.js';
+export * from './stores.js';
+export * from './categories.js';
+export * from './products.js';
+export * from './inventory.js';
+export * from './employees.js';
+export * from './reports.js';

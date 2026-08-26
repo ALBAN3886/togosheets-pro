c6d7507350f127a432411793307bfcd74d2424b8
AET Technology <aettechnology5@gmail.com>
2026-08-26 10:29:02 +0000

    Cleanup: remove obsolete commerce-lot2 (merged into commerce-lot5)

--- commerce-lot2/src/commerce/core/realtime/watch-products.js
+++
@@ -1,9 +0,0 @@
-import * as productsRepo from '../repositories/product.repository.js';
-
-export function watchPublishedProducts(db, tenantId, storeId, onChange) {
-  return productsRepo.watchProducts(db, tenantId, {
-    storeId,
-    isPublished: true,
-    isActive: true,
-  }, onChange);
-}

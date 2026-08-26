0cb4c220c0318bd296131bc75e02ed445ab4f288
AET Technology <aettechnology5@gmail.com>
2026-08-26 10:28:40 +0000

    Cleanup: remove obsolete commerce-lot2 (merged into commerce-lot5)

--- commerce-lot2/src/commerce/apps/admin/pages/index.js
+++
@@ -1,7 +0,0 @@
-export * from './dashboard.js';
-export * from './stores.js';
-export * from './categories.js';
-export * from './products.js';
-export * from './inventory.js';
-export * from './employees.js';
-export * from './reports.js';

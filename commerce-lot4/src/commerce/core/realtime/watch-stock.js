b503e26e5ba0c7bd7d2c85f1bf2b5c4ac9a99c21
AET Technology <aettechnology5@gmail.com>
2026-08-26 10:31:02 +0000

    Cleanup: remove obsolete commerce-lot4 (merged into commerce-lot5)

--- commerce-lot4/src/commerce/core/realtime/watch-stock.js
+++
@@ -1,4 +0,0 @@
-import * as inventoryRepo from '../repositories/inventory.repository.js';
-export function watchStoreStock(db, tenantId, storeId, onChange) {
-  return inventoryRepo.watchInventory(db, tenantId, { storeId }, onChange);
-}
